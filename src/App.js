53f9bc7817e4ff45055fa323c06526969f3f3eb1
GargDivanshu <divanshu.garg113@gmail.com>
2023-05-27 16:45:45 +0530

    beautify

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import UploadPhoto from './components/UploadPhoto';
 
 function App() {
   return (
-    <div>
-      <h1>Upload Photo and Preview</h1>
+    <div className="p-6">
+      <h1 className="text-center text-3xl text-blue-500 font-bold">Upload Photo and Preview</h1>
       <UploadPhoto />
     </div>
   );
--- src/components/UploadPhoto.jsx
+++ src/components/UploadPhoto.jsx
@@ -1,15 +1,41 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef } from "react";
+import { BsUpload } from "react-icons/bs";
+import { RiImageAddFill } from "react-icons/ri";
 
 const UploadPhoto = () => {
   const [selectedFile, setSelectedFile] = useState(null);
   const [previewImage, setPreviewImage] = useState(null);
-  const [imageDimensions, setImageDimensions] = useState({ width: null, height: null });
-  const [originalImage, setOriginalImage] = useState({ width: null, height: null });
+  const [imageDimensions, setImageDimensions] = useState({
+    width: null,
+    height: null,
+  });
+  const [originalImage, setOriginalImage] = useState({
+    width: null,
+    height: null,
+  });
 
   const [draggableData, setDraggableData] = useState([
-    { id: 'title', value: 'Title', position: { x: 0, y: 0 }, isDragging: false, dragStartPosition: { x: 0, y: 0 } },
-    { id: 'date', value: '', position: { x: 0, y: 10 }, isDragging: false, dragStartPosition: { x: 0, y: 0 } },
-    { id: 'link', value: 'Link', position: { x: 0, y: 20 }, isDragging: false, dragStartPosition: { x: 0, y: 0 } },
+    {
+      id: "title",
+      value: "Title",
+      position: { x: 0, y: 0 },
+      isDragging: false,
+      dragStartPosition: { x: 0, y: 0 },
+    },
+    {
+      id: "date",
+      value: "",
+      position: { x: 0, y: 35 },
+      isDragging: false,
+      dragStartPosition: { x: 0, y: 0 },
+    },
+    {
+      id: "link",
+      value: "Link",
+      position: { x: 0, y: 70 },
+      isDragging: false,
+      dragStartPosition: { x: 0, y: 0 },
+    },
   ]);
 
   const containerRef = useRef(null);
@@ -25,23 +51,28 @@ const UploadPhoto = () => {
       const image = new Image();
       image.src = reader.result;
       image.onload = () => {
-        const canvas = document.createElement('canvas');
-        const ctx = canvas.getContext('2d');
+        const canvas = document.createElement("canvas");
+        const ctx = canvas.getContext("2d");
         ctx.drawImage(image, 0, 0, 300, 300);
-        const resizedImageURL = canvas.toDataURL('image/jpeg');
+        const resizedImageURL = canvas.toDataURL("image/jpeg");
 
         const width = image.width;
         const height = image.height;
 
         const widthInMillimeters = (width * 25.4) / 300;
         const heightInMillimeters = (height * 25.4) / 300;
-        setImageDimensions({ width: 300, height: 300, widthInMillimeters, heightInMillimeters });
+        setImageDimensions({
+          width: 300,
+          height: 300,
+          widthInMillimeters,
+          heightInMillimeters,
+        });
         setPreviewImage(resizedImageURL);
         setOriginalImage({ width, height });
 
         const currentDate = getCurrentDate();
         const updatedData = draggableData.map((data) => {
-          if (data.id === 'date') {
+          if (data.id === "date") {
             return { ...data, value: currentDate };
           }
           return data;
@@ -57,7 +88,11 @@ const UploadPhoto = () => {
       event.stopPropagation();
       const updatedData = draggableData.map((data) => {
         if (data.id === id) {
-          return { ...data, isDragging: true, dragStartPosition: { x: event.clientX, y: event.clientY } };
+          return {
+            ...data,
+            isDragging: true,
+            dragStartPosition: { x: event.clientX, y: event.clientY },
+          };
         }
         return data;
       });
@@ -79,14 +114,16 @@ const UploadPhoto = () => {
   };
 
   const handleMouseMove = (event, id) => {
-    event.stopPropagation();
     const updatedData = draggableData.map((data) => {
       if (data.id === id && data.isDragging) {
         const offsetX = event.clientX - data.dragStartPosition.x;
         const offsetY = event.clientY - data.dragStartPosition.y;
         return {
           ...data,
-          position: { x: data.position.x + offsetX, y: data.position.y + offsetY },
+          position: {
+            x: data.position.x + offsetX,
+            y: data.position.y + offsetY,
+          },
           dragStartPosition: { x: event.clientX, y: event.clientY },
         };
       }
@@ -108,31 +145,48 @@ const UploadPhoto = () => {
 
   const getCurrentDate = () => {
     const currentDate = new Date();
-    const day = currentDate.getDate().toString().padStart(2, '0');
-    const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+    const day = currentDate.getDate().toString().padStart(2, "0");
+    const month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
     const year = currentDate.getFullYear().toString().substr(-2);
     return `${day}-${month}-${year}`;
   };
 
   return (
     <div>
-      <input type="file" onChange={handleFileInputChange} />
+     
+
+<input
+  type="file"
+  className="hidden"
+  id="uploadInput"
+  onChange={handleFileInputChange}
+/>
+
+<label htmlFor="uploadInput" className="flex mx-auto w-fit my-8 items-center px-4 py-2 bg-blue-500 text-white rounded-md cursor-pointer">
+  <BsUpload className="mr-2" />
+  Upload File
+</label>
+<div className="grid grid-cols-2 justify-center">
       {previewImage && (
         <div
-          style={{ position: 'relative', width: '300px', height: '300px', overflow: 'hidden' }}
+          className="mx-auto overflow-hidden relative w-[300px] h-[300px]"
           ref={containerRef}
         >
-          <img src={previewImage} alt="Preview" style={{ width: '100%', height: '100%' }} />
+          <img
+            src={previewImage}
+            alt="Preview"
+            style={{ width: "100%", height: "100%" }}
+          />
           {draggableData.map((data) => (
             <div
               key={data.id}
               style={{
-                position: 'absolute',
+                position: "absolute",
                 top: `${data.position.y}px`,
                 left: `${data.position.x}px`,
-                userSelect: 'none',
+                userSelect: "none",
                 zIndex: data.isDragging ? 2 : 1,
-                cursor: data.isDragging ? 'grabbing' : 'grab',
+                cursor: data.isDragging ? "grabbing" : "grab",
               }}
               onMouseDown={(event) => handleMouseDown(event, data.id)}
               onMouseUp={(event) => handleMouseUp(event, data.id)}
@@ -141,31 +195,56 @@ const UploadPhoto = () => {
               <input
                 type="text"
                 value={data.value}
+                className="select-none"
                 style={{
                   margin: 0,
-                  color: 'white',
-                  background: 'rgba(0, 0, 0, 0.5)',
-                  padding: '4px 8px',
-                  border: 'none',
+                  color: "white",
+                  background: "rgba(0, 0, 0, 0.5)",
+                  padding: "4px 8px",
+                  border: "none",
                 }}
-                onChange={(event) => handleInputChange(event, data.id)}
+                readOnly
               />
             </div>
           ))}
         </div>
       )}
-      {imageDimensions.width && (
-        <div>
-          <p>Dimensions (pixels): {imageDimensions.width} x {imageDimensions.height}</p>
-          <p>
-            Original Image Dimensions (pixels): {originalImage.width} x {originalImage.height}
-          </p>
-          <p>
-            Original Dimensions (millimeters): {imageDimensions.widthInMillimeters} x{' '}
-            {imageDimensions.heightInMillimeters}
-          </p>
-        </div>
+
+{previewImage && (
+        <form className="w-fit h-fit p-3 m-3 mx-auto bg-blue-500 rounded-md my-auto">
+          <label>
+            <span className="text-xl font-semibold text-white">Title:</span>
+            <input
+              type="text"
+              className="bg-blue-500 text-white mx-2 border-emphasis outline-none"
+              value={draggableData.find((data) => data.id === "title").value}
+              onChange={(event) => handleInputChange(event, "title")}
+            />
+          </label>
+          <br />
+          <label>
+            <span className="text-xl font-semibold text-white">Date:</span>
+            <input
+              className="bg-blue-500  text-white mx-2 border-emphasis outline-none"
+              type="text"
+              value={draggableData.find((data) => data.id === "date").value}
+              onChange={(event) => handleInputChange(event, "date")}
+            />
+          </label>
+          <br />
+          <label>
+            <span className="text-xl font-semibold text-white">Link:</span>
+            <input
+              className="bg-blue-500  text-white mx-2 border-emphasis outline-none"
+              type="text"
+              value={draggableData.find((data) => data.id === "link").value}
+              onChange={(event) => handleInputChange(event, "link")}
+            />
+          </label>
+        </form>
       )}
+
+</div>
     </div>
   );
 };
