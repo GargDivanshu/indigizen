6cf56bcc60861eb10ac1cad7bb88d64bf6e42360
GargDivanshu <divanshu.garg113@gmail.com>
2023-06-01 19:09:16 +0530

    text field resize

--- src/components/UploadPhoto.jsx
+++ src/components/UploadPhoto.jsx
@@ -5,6 +5,9 @@ import { RiImageAddFill } from "react-icons/ri";
 const UploadPhoto = () => {
   const [selectedFile, setSelectedFile] = useState(null);
   const [previewImage, setPreviewImage] = useState(null);
+  const [originalImageDimensions, setOriginalImageDimensions] = useState({ width: 0, height: 0 });
+const [newImageDimensions, setNewImageDimensions] = useState({ width: 0, height: 0 });
+
   const [draggableData, setDraggableData] = useState([
     {
       id: "title",
@@ -12,6 +15,8 @@ const UploadPhoto = () => {
       position: { x: 0, y: 0 },
       isDragging: false,
       dragStartPosition: { x: 0, y: 0 },
+      width: 100,
+      height: 20,
     },
     {
       id: "date",
@@ -19,6 +24,8 @@ const UploadPhoto = () => {
       position: { x: 0, y: 35 },
       isDragging: false,
       dragStartPosition: { x: 0, y: 0 },
+      width: 100,
+      height: 20,
     },
     {
       id: "link",
@@ -26,6 +33,8 @@ const UploadPhoto = () => {
       position: { x: 0, y: 70 },
       isDragging: false,
       dragStartPosition: { x: 0, y: 0 },
+      width: 100,
+      height: 20,
     },
   ]);
 
@@ -39,7 +48,7 @@ const UploadPhoto = () => {
       image.onload = () => {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext("2d");
-
+  
         const aspectRatio = image.width / image.height;
         let width, height;
         if (aspectRatio > 1) {
@@ -49,14 +58,21 @@ const UploadPhoto = () => {
           height = 300;
           width = height * aspectRatio;
         }
-
+  
         canvas.width = 300;
         canvas.height = 300;
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.drawImage(image, 0, 0, width, height);
+  
+        // Set the original image dimensions
+        setOriginalImageDimensions({ width: image.width, height: image.height });
+        console.log(width + " "  + height)
+        setNewImageDimensions({ width: width, height: height });
+
       };
     }
   }, [previewImage]);
+  
 
   const handleFileInputChange = (event) => {
     const file = event.target.files[0];
@@ -145,6 +161,28 @@ const UploadPhoto = () => {
     setDraggableData((prevData) => [...prevData, newField]);
   };
 
+  const handleWidthChange = (event, id) => {
+    const width = event.target.value;
+    const updatedData = draggableData.map((data) => {
+      if (data.id === id) {
+        return { ...data, width };
+      }
+      return data;
+    });
+    setDraggableData(updatedData);
+  };
+  
+  const handleHeightChange = (event, id) => {
+    const height = event.target.value;
+    const updatedData = draggableData.map((data) => {
+      if (data.id === id) {
+        return { ...data, height };
+      }
+      return data;
+    });
+    setDraggableData(updatedData);
+  };
+
   return (
     <div>
       <input type="file" className="hidden" id="uploadInput" onChange={handleFileInputChange} />
@@ -156,7 +194,7 @@ const UploadPhoto = () => {
         <BsUpload className="mr-2" />
         Upload File
       </label>
-      <div className="grid md:grid-cols-2 sm:grid-cols-1 justify-center my-5">
+      <div className="grid md:grid-cols-2 sm:grid-cols-1 justify-around my-5">
         {previewImage && (
           <div
             className="mx-auto overflow-hidden relative w-[300px] h-[300px]"
@@ -189,6 +227,8 @@ const UploadPhoto = () => {
                     margin: 0,
                     color: "white",
                     background: "rgba(0, 0, 0, 0.5)",
+                    width: `${data.width}px`,
+                    height: `${data.height}px`,
                     padding: "4px 8px",
                     border: "none",
                   }}
@@ -199,33 +239,137 @@ const UploadPhoto = () => {
           </div>
         )}
 
-        {previewImage && (
-          <div className="mx-auto">
-            <div className="flex items-center">
-              <RiImageAddFill className="mr-2" />
-              <button
-                className="px-4 py-2 bg-blue-500 text-white rounded-md"
-                onClick={handleAddField}
-              >
-                Add Field
-              </button>
-            </div>
-            {draggableData.map((data) => (
-              <div className="my-2 flex" key={data.id}>
-                <input
-                  type="text"
-                  value={data.value}
-                  onChange={(event) => handleInputChange(event, data.id)}
-                  className="px-2 py-1 border rounded-md"
-                />
-                <span className="text-sm mr-2">
+{previewImage && (
+  <div className="mx-auto">
+    <div className="flex items-center">
+      <RiImageAddFill className="mr-2" />
+      <button
+        className="px-4 py-2 bg-blue-500 text-white rounded-md"
+        onClick={handleAddField}
+      >
+        Add Field
+      </button>
+    </div>
+    {draggableData.map((data) => (
+  <div className="my-2 flex" key={data.id}>
+    <button
+      className="border-2 border-blue-500 rounded-md px-2 py-1 mr-2 text-blue-500 hover:bg-blue-500 hover:text-white"
+      onClick={() => {
+        const newData = draggableData.map((item) => {
+          if (item.id === data.id) {
+            return {
+              ...item,
+              position: { x: 0, y: 0 },
+            };
+          }
+          return item;
+        });
+
+        setDraggableData(newData);
+      }}
+    >
+      reset
+    </button>
+    <input
+      type="text"
+      value={data.value}
+      onChange={(event) => handleInputChange(event, data.id)}
+      className="px-2 py-1 border rounded-md"
+    />
+    <span className="text-sm mx-2 min-w-[60px]">
       ({data.position.x}, {data.position.y})
     </span>
-              </div>
-            ))}
-          </div>
-        )}
+    <div className="flex">
+      
+      <input
+        type="text"
+        value={data.width}
+        onChange={(event) => handleWidthChange(event, data.id)}
+        className="px-2 border rounded-md"
+        style={{ width: "50px" }}
+      />
+      <div className="flex flex-col mx-2">
+      <button
+        className="px-2 border rounded-md text-blue-500"
+        onClick={() => {
+          const newValue = parseInt(data.width) + 1;
+          handleWidthChange({ target: { value: newValue } }, data.id);
+        }}
+      >
+        ▲
+      </button>
+
+      <button
+        className="px-2 border rounded-md text-blue-500"
+        onClick={() => {
+          const newValue = parseInt(data.width) - 1;
+          handleWidthChange({ target: { value: newValue } }, data.id);
+        }}
+      >
+        ▼
+      </button>
       </div>
+      
+    </div>
+    <span className="text-sm mx-2 justify-center m-auto">Width </span>
+    <div className="flex">
+      
+      <input
+        type="text"
+        value={data.height}
+        onChange={(event) => handleHeightChange(event, data.id)}
+        className="px-2 border rounded-md"
+        style={{ width: "50px" }}
+      />
+      <div className="flex flex-col mx-2">
+      <button
+        className="px-2 border rounded-md text-blue-500"
+        onClick={() => {
+          const newValue = parseInt(data.height) + 1;
+          handleHeightChange({ target: { value: newValue } }, data.id);
+        }}
+      >
+        ▲
+      </button>
+
+      <button
+        className="px-2 border rounded-md text-blue-500"
+        onClick={() => {
+          const newValue = parseInt(data.height) - 1;
+          handleHeightChange({ target: { value: newValue } }, data.id);
+        }}
+      >
+        ▼
+      </button>
+      </div>
+      
+    </div>
+    <span className="text-sm mx-2 justify-center m-auto">Height </span>
+  </div>
+))}
+
+
+
+  </div>
+)}
+      </div>
+
+      {previewImage && (
+    <div>
+      
+
+      <div className="my-4 text-center">
+        Original Image Dimensions: {originalImageDimensions.width}px x {originalImageDimensions.height}px
+      </div>
+
+      <div className="my-4 text-center">
+        New Image Dimensions: {newImageDimensions.width}px x {newImageDimensions.height}px
+      </div>
+
+
+    </div>
+  )}
+
     </div>
   );
 };
